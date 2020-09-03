f5fc9d918b3e4a757def7b425ff6a66aa5d56b58
Manoj Moktan <manojmoktandeveloper@gmail.com>
2020-09-03 14:47:05 +0545

    prepare  for deploy production mode through heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
 //Connect Database
 connectDB();
 
@@ -14,7 +15,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/post', require('./routes/api/post'));
 app.use('/api/profile', require('./routes/api/profile'));
 
-
+//Serve static assets in production.
+if (process.env.NODE_ENV==='production') {
+    // set static folder
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
